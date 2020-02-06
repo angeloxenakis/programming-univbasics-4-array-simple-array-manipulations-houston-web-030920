6de46190081dcb4365a10ac0c5391616f5137224
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-06 18:33:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,8 +27,11 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert[4](element)
+  array.insert(4, element)
 end
+
+def using_delete(array, element)
+  array.delete(element)
 
 def using_uniq(array)
   array.uniq 
